4d3e1d10209588ef7828f336c736e0acee17aa9c
Ivan-ux-dot <ivansimonovics4@gmail.com>
2021-05-12 17:57:44 +0200

    Create resenje2.php

---
+++ Nedelja4/Domaci_12/zadatak_2/resenje2.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+    $green="green";
+    $blue="blue";
+    echo "<p style='color:green'>$green</p>";
+    echo "<p style='color:blue'>$blue </p>";
+
+    echo"<a Href='../zadatak_3/resenje3.php'>Link3</a>";
+    ?>
+</body>
+</html>
