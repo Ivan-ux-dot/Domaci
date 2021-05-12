50e7576ebdf56fe3ed0746765dab86ebd27127c2
Ivan-ux-dot <ivansimonovics4@gmail.com>
2021-05-12 14:43:42 +0200

    Create vezba.php

---
+++ Nedelja4/11D_2308_Ivan_Simonovic/vezba.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $p1=200;
+    $p2=80;
+    $zbir=$p1+$p2;
+    echo $zbir;
+    echo "<br>";
+    $proidzvod=$p1*$p2;
+    echo $proidzvod;
+    echo "<br>";
+    $razlika=$p1-$p2;
+    echo $razlika;
+    echo "<br>";
+    $kolicnik=$p1/$p2;
+    echo $kolicnik."<br>";
+
+    $a=1;
+    $b=2;
+    $c=3;
+    $rezultat=$a+$b+$c;
+    echo "<p>Zbir brojeva 1,2 i 3 je ". ($rezultat)."</p>";
+    
+
+?>
+</body>
+</html>
