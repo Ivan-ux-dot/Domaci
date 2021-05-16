709282b645493bb06a747d4aa9d0a72a06612646
Ivan-ux-dot <ivansimonovics4@gmail.com>
2021-05-16 17:47:38 +0200

    Create forma.php

---
+++ Nedelja4/Domaci_14/forma.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+  <form action="forma2.php" target="zad1" >
+  <Input type="text" name="ime"/>
+  <input type="text" name="prezime"/>
+  <input type="submit" value="salji"/>
+  </form>
+  <iframe name="zad1"></iframe>
+  </body>
+  </html>
+  
