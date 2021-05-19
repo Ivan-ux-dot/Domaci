676568c068d51ca57aba88010c933a3646ec85c4
Ivan-ux-dot <ivansimonovics4@gmail.com>
2021-05-19 18:04:23 +0200

    Create zad.php

---
+++ Nedelja5/Domaci_16/zad.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <form action="zad1.php">
+    <input type="number" name="broj1"/>
+    <input type="number" name="broj2"/>
+    <input type="number" name="broj3"/>
+    <input type="number" name="broj4"/>
+    <input type="submit" value="posalji"/>
+    </form>
+</body>
+</html>
