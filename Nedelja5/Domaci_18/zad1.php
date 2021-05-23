abe21bf4c8271fa9a0ac8d2bafd742c2b8967a8c
Ivan-ux-dot <ivansimonovics4@gmail.com>
2021-05-23 18:11:11 +0200

    Create zad1.php

---
+++ Nedelja5/Domaci_18/zad1.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<!-- Ispisati brojeve od 20 do 1 preko while ciklusa. -->
+<?php
+$t=1;
+$a=21;
+while($a>1){
+    $a=$a-$t;
+    echo $a;
+}
+    
+
+?>
+</body>
+</html>
